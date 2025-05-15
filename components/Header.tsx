55e20f85b9549cbd1c742673b176841e6ba365ac
FarCodeFR <Timo-44@hotmail.com>
2025-05-16 01:52:24 +0200

    Ajout d'un menu burger et de nouvelles images

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,37 @@
+"use client";
+import "../styles/burger-menu.css";
+import { useState } from "react";
+import NavLink from "next/link";
+
 function Header() {
-  return <h1>Header</h1>;
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  return (
+    <header>
+      <button className="burger-button" onClick={toggleMenu}>
+        <img src={isOpen ? "/images/close.svg" : "/images/menu.svg"}></img>
+      </button>
+      <div className={`dropdown-menu ${isOpen ? "open" : ""}`}>
+        <nav className="menu-item">
+          <ul>
+            <li>
+              <NavLink href={""}>À Propos</NavLink>
+            </li>
+            <li>
+              <NavLink href={""}>Les jeux</NavLink>
+            </li>
+            <li>
+              <NavLink href={""}>Nous aider</NavLink>
+            </li>
+            <li>
+              <NavLink href={""}>Contact</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
