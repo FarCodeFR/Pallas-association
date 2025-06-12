9cadb366d69ad69a637e51e1f5ea2e2bdb0bbbe0
FarCodeFR <Timo-44@hotmail.com>
2025-06-12 15:25:14 +0200

    Add picture and fix home burger-menue footer help and office

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,10 +11,16 @@ function Footer() {
         height={100}
       />
       <p>
-        Mission et statut associatifs
-        <br /> loi 1901 <br />
+        Pallas éducation & ludopédagogie
+        <br />
         <br /> 12 rue Traversière 44190 Clisson <br />
         SIRET: 923 088 124 00014 <br /> RNA: W442027986
+      </p>
+      <p>
+        Pallas ludopédagogie est une association loi 1901 qui a pour objet de
+        réduire les inégalités d&#39;accès aux savoirs humains essentiels en
+        proposant des activités de sensibilisation par le jeu aux bénéficiaires,
+        principalement des jeunes.
       </p>
     </footer>
   );
